bc269ae30c6434f2b960a56a5d3601dfe52b92a9
danielbarnes175 <danielbarnes175@users.noreply.github.com>
2022-10-12 20:28:25 +0000

    deploy: fb4fe5b1e1b2eecc5c0ff4b9410742a2c361c3f6

--- index.js
+++ index.js
@@ -11,3 +11,25 @@ function toggleClassTable(classId) {
 
   currentClassShown = classId;
 }
+
+function filterTable() {
+    // Declare variables
+    let input = document.getElementById("filter");
+    let filter = input.value.toUpperCase();
+    let table = document.getElementById(currentClassShown);
+    let tr = table.getElementsByTagName("tr");
+    let index = document.getElementById("filterCategory").selectedIndex;
+  
+    // Loop through all table rows, and hide those who don't match the search query
+    for (let i = 0; i < tr.length; i++) {
+      let td = tr[i].getElementsByTagName("td")[index];
+      if (td) {
+        let txtValue = td.textContent || td.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          tr[i].style.display = "";
+        } else {
+          tr[i].style.display = "none";
+        }
+      }
+    }
+  }
