c6394c13075783cec1dd485f66eaf9db87df7f52
Daniel Barnes <danielbarnes175@gmail.com>
2022-10-12 16:26:44 -0700

    Make logs clickable

--- render.js
+++ render.js
@@ -1,6 +1,16 @@
 const fs = require('fs');
 const Handlebars = require('handlebars');
 
+Handlebars.registerHelper('splitLogs', function(logString) {
+  var logs = logString.split(", ");
+
+  let logList = '';
+  for (const logId of logs) {
+    logList += `<a href="https://logs.tf/${logId}">${logId}</a>, `
+  }
+  return logList;
+});
+
 function render(filename, data)
 {
   let source   = fs.readFileSync(filename,'utf8').toString();
@@ -10,6 +20,5 @@ function render(filename, data)
 }
 
 let data = JSON.parse(fs.readFileSync("./totalPlayerStats.json", 'utf8'));
-
 let result = render('./templates/index.hbs', data);
 fs.writeFileSync("public/index.html", result);
